6440ee9c442f41619dadcb7b63b5b4f7abfd1344
week4-sigma <mtn.oppo59913@gmail.com>
2024-06-14 15:34:05 +0700

    first commit

---
+++ app401.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+    printf("Welcome to ITTRAT");
+    printf("Department of Information Tehnology");
+    return (0);
+}
---
+++ app402.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main()
+{
+    printf("%s %5d %.2f","hello",12,20.5);
+}
